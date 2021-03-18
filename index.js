01aa47c1ce3af7e09b6850a530bd08ba6669b4d2
Pau Charques Rius <paucharquesrius@Paus-MBP.home>
2021-03-18 12:00:29 +0100

    sad

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.get('/api', (req, res) => res.send('Its working!'));
 
-app.listen(process.env.port || 4000, function(){
+app.listen(process.env.port || 3000, function(){
     console.log('now listening for requests');
  });
 
