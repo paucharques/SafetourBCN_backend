ff8e093e96deb31cc6c15da5877a8684774ea702
Albert <albertbert03@gmail.com>
2021-03-18 19:08:39 +0100

    Treballant amb post request

--- index.js
+++ index.js
@@ -65,15 +65,13 @@ app.post('/register/', async (request, response) => {
     var username = post_data.username;
     var password = post_data.password;
 
-    console.log(request.body.id);
-    console.log(request.body.username);
-    console.log(request.body.password);
-
     conn = await pool.getConnection();
     
+    //S'hauria de comprovar que no existeix el username i assginar l'id automaticament
+
     conn.query('INSERT INTO USUARIOS VALUES(?,?,?);', [id, username, password], (error, result) => {
         if (error) throw error;
- 
+        
         response.status(201).send(`User added with ID: ${result.insertId}`);
     });
 });
