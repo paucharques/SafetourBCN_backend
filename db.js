ec63567aae71e72e9d3a052ee7270822c8093a5e
Pau Charques Rius <paucharquesrius@Paus-MBP.home>
2021-03-18 14:00:46 +0100

    bugs

--- db.js
+++ db.js
@@ -3,7 +3,8 @@ var mariadb = require('mariadb');
 
 // create a new connection pool
 const pool = mariadb.createPool({
-    host: 'localhost', 
+    host: 'localhost',
+    port: 3306,
     user:'root', 
     password: 'safetour2021',
     connectionLimit: 5
