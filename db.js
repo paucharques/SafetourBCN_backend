f7c1aeac04a2e227e7bdada9815d99d901345221
Pau Charques Rius <paucharquesrius@Paus-MBP.home>
2021-03-18 13:26:53 +0100

    new file

---
+++ db.js
@@ -0,0 +1,23 @@
+// import mariadb
+var mariadb = require('mariadb');
+
+// create a new connection pool
+const pool = mariadb.createPool({
+    host: 'localhost', 
+    user:'root', 
+    password: 'safetour2021',
+    connectionLimit: 5
+});
+
+// expose the ability to create new connections
+module.exports={
+    getConnection: function(){
+      return new Promise(function(resolve,reject){
+        pool.getConnection().then(function(connection){
+          resolve(connection);
+        }).catch(function(error){
+          reject(error);
+        });
+      });
+    }
+  } 
