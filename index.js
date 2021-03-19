49b921dc8f9fb4c0d55caf5c7790b63b9144241d
Albert <albertbert03@gmail.com>
2021-03-19 20:21:00 +0100

    arreglant post

--- index.js
+++ index.js
@@ -56,22 +56,26 @@ app.get('/usuarios/:id', async (req, res) => {
 /**********************POST***********************/
 
 // Add a new user
-app.post('/register', async (request, response) => {
+app.post('/register', async (req, res) => {
     let conn;
     try{
         conn = await pool.getConnection();
 
-        var post_data = request.body;
+        var post_data = req.body;
         var id = post_data.id;
         var username = post_data.username;
         var password = post_data.password;
     
         //S'hauria de comprovar que no existeix el username i assginar l'id automaticament
-        conn.query('INSERT INTO USUARIOS VALUES(?,?,?);', [id, username, password], (error, result) => {
-            if (error) throw error;
-            
-            response.status(201).send('user added');
+        conn.query('INSERT INTO USUARIOS VALUES(?,?,?);', [id, username, password])
+        .then((result) => {
+            console.log("Arriba aqui");
+            res.status(201).send('user added');
+        })
+        .catch(err => {
+            throw err
         });
+
     }catch(err){
         throw err;
     } finally {
