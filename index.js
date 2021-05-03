5fa1fdade6defa57f24d6aa96ee373f0a39a7229
Albert <albertbert03@gmail.com>
2021-05-03 19:09:39 +0200

    arreglant login

--- index.js
+++ index.js
@@ -174,6 +174,7 @@ app.get("/login", async (req, res) => {
     var password = req.body.password;
 
     var rows = conn.query("SELECT * FROM USERS WHERE EMAIL = ? AND PASSWORD = ?;", [email,password]);
+    res.send(rows);
 
   }catch{
     res.status(500).send("Error connecting db");
