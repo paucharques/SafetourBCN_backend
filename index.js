45a6f511a7a253d74ff802b8d5d0d168762b3811
Pau Charques Rius <paucharquesrius@Paus-MBP.home>
2021-05-10 21:30:41 +0200

    Added CORS compatibility

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
 
 app.get("/api", (req, res) => res.send("Its working!"));
 
