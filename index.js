25cce88e3252cf26d4252fd5c582127c25bc77c0
Pau Charques Rius <paucharquesrius@Paus-MacBook-Pro.local>
2021-05-10 12:05:39 +0200

    ewew

--- index.js
+++ index.js
@@ -165,7 +165,10 @@ app.get("/user/login", async (req, res) => {
     var email = req.body.email;
     var password = req.body.password;
 
-    var rows = await conn.query("select * from USERS", [email, password]);
+    var rows = await conn.query(
+      "select * from USERS u INNER JOIN INDIVIDUAL_USER iu ON u.EMAIL = iu.EMAIL",
+      [email, password]
+    );
   } catch {
     res.status(500).send("Error connecting db");
   }
