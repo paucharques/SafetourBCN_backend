0932e517f289a05c01f0ae0bedf06f93080007ad
ALUMNE <alumne@venusaur.fib.upc.es>
2021-05-10 19:49:26 +0000

    Corswq wq

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require('cors');
 const bodyParser = require("body-parser");
 var cookieParser = require("cookie-parser");
 const pool = require("./db");
@@ -8,16 +9,8 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
+app.use(cors());
+
 
 app.get("/api", (req, res) => res.send("Its working!"));
 
