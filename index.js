01755702dee56e27f1160da17d690450d49590a3
Albert <albertbert03@gmail.com>
2021-05-13 20:28:43 +0200

    provant myestablishments

--- index.js
+++ index.js
@@ -186,7 +186,7 @@ app.get("/company/:email/establishments", async (req, res) => {
 app.get("/myestablishments", authenticateJWT, async (req, res) => {
   let conn;
   var email = jwt.decode(req.headers.authorization);
-
+  console.log(email)
   try {
     // establish a connection to MariaDB
     conn = await pool.getConnection();
