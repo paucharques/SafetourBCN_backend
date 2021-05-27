f239c4f4a4ca975fd8c1f8b859a3b2f1fc9ef29c
dawid.aleksander.sanecki <dawid.aleksander.sanecki@est.fib.upc.edu>
2021-05-27 02:19:33 +0200

    get reviews de un establishment

--- index.js
+++ index.js
@@ -257,6 +257,25 @@ app.get("/Events", async (req, res) => {
   }
 });
 
+app.get("/Establishment/:id/Reviews", async (req, res) => {
+  let conn;
+  try {
+    // establish a connection to MariaDB
+    conn = await pool.getConnection();
+
+    // execute the query and set the result to a new variable
+    var rows = await conn.query("select * from REVIEWS where ID_ESTABLISHMENT = ?", [
+      req.params.id,
+    ]);
+  } catch {
+    res.status(500).send(err);
+  } finally {
+    // return the results
+    res.status(200).send(rows);
+    if (conn) return conn.release();
+  }
+});
+
 /**********************POST***********************/
 
 // LOGIN individual users
