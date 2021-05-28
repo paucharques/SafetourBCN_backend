302d1a9d008b65bd04cab05fe42972f29b198c2b
dawid.aleksander.sanecki <dawid.aleksander.sanecki@est.fib.upc.edu>
2021-05-28 02:31:15 +0200

    Implemento AverageRating que devuelve una media de ratings

--- index.js
+++ index.js
@@ -276,6 +276,25 @@ app.get("/Establishment/:id/Ratings", async (req, res) => {
   }
 });
 
+app.get("/Establishment/:id/AverageRating", async (req, res) => {
+  let conn;
+  try {
+    // establish a connection to MariaDB
+    conn = await pool.getConnection();
+
+    // execute the query and set the result to a new variable
+    var rows = await conn.query("select AVG(VALUE) from RATINGS where ID_ESTABLISHMENT = ?", [
+      req.params.id,
+    ]);
+  } catch {
+    res.status(500).send(err);
+  } finally {
+    // return the results
+    res.status(200).send(rows);
+    if (conn) return conn.release();
+  }
+});
+
 app.get("/UserRatings", authenticateJWT, async (req, res) => {
   let conn;
   try {
