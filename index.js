16e1270c336c599d564e112c4417829d24dfda5e
dawid.aleksander.sanecki <dawid.aleksander.sanecki@est.fib.upc.edu>
2021-06-02 03:59:00 +0200

    fix?

--- index.js
+++ index.js
@@ -541,7 +541,7 @@ app.post("/registerEstablishment", authenticateJWT, async (req, res) => {
       .query(
         "INSERT INTO ESTABLISHMENTS (OWNER,LOCAL_X,LOCAL_Y,DESCRIPTION,MAX_CAPACITY, HOUROPEN, HOURCLOSE, NAME, CATEGORY, PRICE, DISCOUNT, ADDRESS, WEBSITE, INSTAGRAM) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?);",
         [
-          req.user.username,
+          req.user.owner,
           req.body.local_x,
           req.body.local_y,
           req.body.description,
