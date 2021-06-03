f6f6a9a67d84941f000a1c500e82edf268c12856
dawid.aleksander.sanecki <dawid.aleksander.sanecki@est.fib.upc.edu>
2021-06-03 04:41:49 +0200

    fix3

--- index.js
+++ index.js
@@ -1255,7 +1255,7 @@ app.put("/establishment/:id", authenticateJWT, async (req, res) => {
     conn = await pool.getConnection();
     conn
       .query(
-        "UPDATE ESTABLISHMENT SET "+req.body.parameter+ " = ? WHERE ID_ESTABLISHMENT = ? AND OWNER = ?",
+        "UPDATE ESTABLISHMENTS SET "+req.body.parameter+ " = ? WHERE ID_ESTABLISHMENT = ? AND OWNER = ?",
         [
         req.body.value,
         req.params.id,
